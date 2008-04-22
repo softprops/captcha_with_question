2c26feb41cfaba2e5f63458d2a5503ce6663a4da
Doug <doug@Macintosh.local>
2008-04-21 21:32:46 -0400

    updated readme

--- test/captcha_with_question_test.rb
+++ test/captcha_with_question_test.rb
@@ -1,6 +1,6 @@
 require 'test/unit'
 
-class IsQuestionableTest < Test::Unit::TestCase
+class CaptchaWithQuestionTest < Test::Unit::TestCase
   # Replace this with your real tests.
   def test_this_plugin
     flunk
